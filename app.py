82cc4bd33b1124445240db5366a46abcae1aca49
Noa0910 <nosorioarias10@gmail.com>
2024-09-08 22:21:01 -0500

    upload xlsx files code

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ import os
 
 
 load_dotenv()
+
+st.title('Upload and Merge Patient and Responsible Party Files')
+
+st.write("Upload the Patient and Responsible Party files (both files are required).")
+
+uploaded_files = st.file_uploader("Choose the Patient and Responsible Party files", type=["xlsx"], accept_multiple_files=True)
